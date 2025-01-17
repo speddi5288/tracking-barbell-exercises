c0f256dba005176aa55e2293b18b9355057d8582
Srisaketh Peddi <srisaketh.peddi@gmail.com>
2025-01-17 17:39:18 -0500

    Updates build features to include 3d clustering & fourier transformations

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -3,12 +3,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 from DataTransformation import LowPassFilter, PrincipalComponentAnalysis
 from TemporalAbstraction import NumericalAbstraction
+from FrequencyAbstraction import FourierTransformation
+from sklearn.cluster import KMeans
 
 
 # --------------------------------------------------------------
 # Load data
 # --------------------------------------------------------------
-
 df = pd.read_pickle("../../data/interim/02_outliers_removed_chauvenets.pkl")
 
 predictor_columns = list(df.columns[:6])
@@ -19,12 +20,10 @@ plt.rcParams["figure.figsize"] = (20, 5)
 plt.rcParams["figure.dpi"] = 100
 plt.rcParams["lines.linewidth"] = 2
 
+df.info()
 # --------------------------------------------------------------
 # Dealing with missing values (imputation)
 # --------------------------------------------------------------
-
-# subset of a dataframe and set equal to a specific set
-# subset = df[df["set"] == 35] ["gyr_y"].plot()
 
 for col in predictor_columns:
     df[col] = df[col].interpolate()
@@ -35,8 +34,10 @@ df.info()
 # Calculating set duration
 # --------------------------------------------------------------
 
-df[df["set"] == 25]["acc_y"].plot()
-df[df["set"] == 60]["acc_y"].plot()
+df[df["set"] == 50]["label"]
+df[df["set"] == 50]["acc_y"].plot()
+df[df["set"] == 50]["acc_z"].plot()
+df[df["set"] == 50]["acc_x"].plot()
 
 duration = df[df["set"] == 1].index[-1] - df[df["set"] == 1].index[0]
 duration.seconds
@@ -51,19 +52,20 @@ for s in df["set"].unique():
 
 duration_df = df.groupby(["category"])["duration"].mean()
 
-# divided by the amt of reps
 duration_df.iloc[0] / 5
-duration_df.iloc[1] / 10
+duration_df.iloc[0] / 10
+
+# -- Remove the duration column --
+df.drop(columns=["duration"], inplace=True)
 
 # --------------------------------------------------------------
-# Butterworth lowpass filter (makes lines smooter -> overall movement pattern)
+# Butterworth lowpass filter
 # --------------------------------------------------------------
-
 df_lowpass = df.copy()
 LowPass = LowPassFilter()
 
 fs = 1000 / 200
-cutoff = 1.3
+cutoff = 1.2
 
 df_lowpass = LowPass.low_pass_filter(df_lowpass, "acc_y", fs, cutoff, order=5)
 
@@ -81,11 +83,9 @@ for col in predictor_columns:
     df_lowpass[col] = df_lowpass[col + "_lowpass"]
     del df_lowpass[col + "_lowpass"]
 
-
 # --------------------------------------------------------------
 # Principal component analysis PCA
 # --------------------------------------------------------------
-
 df_pca = df_lowpass.copy()
 PCA = PrincipalComponentAnalysis()
 
@@ -93,14 +93,13 @@ pc_values = PCA.determine_pc_explained_variance(df_pca, predictor_columns)
 
 plt.figure(figsize=(10, 10))
 plt.plot(range(1, len(predictor_columns) + 1), pc_values)
-plt.xlabel("principal component number")
+plt.xlabel("principle component number")
 plt.ylabel("explained variance")
 plt.show()
 
 df_pca = PCA.apply_pca(df_pca, predictor_columns, 3)
 
 subset = df_pca[df_pca["set"] == 35]
-
 subset[["pca_1", "pca_2", "pca_3"]].plot()
 
 # --------------------------------------------------------------
@@ -116,32 +115,150 @@ df_squared["acc_r"] = np.sqrt(acc_r)
 df_squared["gyr_r"] = np.sqrt(gyr_r)
 
 subset = df_squared[df_squared["set"] == 14]
-
 subset[["acc_r", "gyr_r"]].plot(subplots=True)
-
-
 
 
 # --------------------------------------------------------------
 # Temporal abstraction
 # --------------------------------------------------------------
+df_temporal = df_squared.copy()
+NumAbs = NumericalAbstraction()
 
+predictor_columns = predictor_columns + ["acc_r", "gyr_r"]
+
+ws = int(1000 / 200)
+for col in predictor_columns:
+    df_temporal = NumAbs.abstract_numerical(df_temporal, [col], ws, "mean")
+    df_temporal = NumAbs.abstract_numerical(df_temporal, [col], ws, "std")
+
+df_temporal_list = []
+for s in df_temporal["set"].unique():
+
+    subset = df_temporal[df_temporal["set"] == s].copy()
+
+    for col in predictor_columns:
+
+        subset = NumAbs.abstract_numerical(subset, [col], ws, "mean")
+        subset = NumAbs.abstract_numerical(subset, [col], ws, "std")
+
+    df_temporal_list.append(subset)
+
+df_temporal = pd.concat(df_temporal_list)
+
+subset[["acc_y", "acc_y_temp_mean_ws_5", "acc_y_temp_std_ws_5"]].plot()
+subset[["acc_y", "acc_y_temp_mean_ws_5", "acc_y_temp_std_ws_5"]].reset_index(
+    drop=True
+).plot()
+subset[["gyr_y", "gyr_y_temp_mean_ws_5", "gyr_y_temp_std_ws_5"]].plot()
 
 # --------------------------------------------------------------
 # Frequency features
 # --------------------------------------------------------------
 
+df_freq = df_temporal.copy().reset_index()
+FreqAbs = FourierTransformation()
+
+# Sampling rate - no. of samples per sec
+fs = int(1000 / 200)
+# Window Size
+ws = int(2800 / 200)  # avg length of repition was 2.8s
+
+df_freq = FreqAbs.abstract_frequency(df_freq, ["acc_y"], ws, fs)
+
+# Visualize Results
+subset = df_freq[df_freq["set"] == 15]
+subset[["acc_y"]].plot()
+subset[
+    [
+        "acc_y",
+        "acc_y_freq_weighted",
+        "acc_y_pse",
+        "acc_y_freq_1.429_Hz_ws_14",
+        "acc_y_freq_2.5_Hz_ws_14",
+    ]
+].plot()
+# pse - power spectral entropy
+
+
+df_freq_list = []
+
+for s in df_freq["set"].unique():
+    print(f"Applying Fourier Transformation to set - {s}")
+
+    subset = df_freq[df_freq["set"] == s].reset_index(drop=True).copy()
+    subset = FreqAbs.abstract_frequency(subset, predictor_columns, ws, fs)
+
+    df_freq_list.append(subset)
+
+df_freq = pd.concat(df_freq_list).set_index("epoch (ms)", drop=True)
+df_freq.info()
 
 # --------------------------------------------------------------
 # Dealing with overlapping windows
 # --------------------------------------------------------------
 
+df_freq = df_freq.dropna()
+df_freq = df_freq.iloc[::2]
 
 # --------------------------------------------------------------
 # Clustering
 # --------------------------------------------------------------
 
 
+df_cluster = df_freq.copy()
+
+cluster_columns = ["acc_x", "acc_y", "acc_z"]
+k_values = range(2, 10)
+inertias = []
+
+for k in k_values:
+    subset = df_cluster[cluster_columns]
+    kmeans = KMeans(n_clusters=k, n_init=20, random_state=0)
+    cluster_labels = kmeans.fit_predict(subset)
+    inertias.append(kmeans.inertia_)
+
+plt.figure(figsize=(10, 10))
+plt.plot(k_values, inertias)
+plt.xlabel("k")
+plt.ylabel("Sum of Squared Distances")
+plt.show()
+    # k = 5 is the "elbow"
+
+kmeans = KMeans(n_clusters=5, n_init=20, random_state=0)
+subset = df_cluster[cluster_columns]
+df_cluster["cluster"] = kmeans.fit_predict(subset)|
+
+# Plot clusters
+fig = plt.figure(figsize=(15, 15))
+ax = fig.add_subplot(projection="3d")
+
+for c in df_cluster["cluster"].unique():
+    subset = df_cluster[df_cluster["cluster"] == c]
+    ax.scatter(subset["acc_x"], subset["acc_y"], subset["acc_z"], label=c)
+    
+ax.set_xlabel("X-axis")
+ax.set_ylabel("Y-axis")
+ax.set_zlabel("Z-axis")
+plt.legend()
+plt.show()
+
+
+# Plot accelerometer data to compare
+fig = plt.figure(figsize=(15, 15))
+ax = fig.add_subplot(projection="3d")
+
+for l in df_cluster["label"].unique():
+    subset = df_cluster[df_cluster["label"] == l]
+    ax.scatter(subset["acc_x"], subset["acc_y"], subset["acc_z"], label=l)
+    
+ax.set_xlabel("X-axis")
+ax.set_ylabel("Y-axis")
+ax.set_zlabel("Z-axis")
+plt.legend()
+plt.show()
+
 # --------------------------------------------------------------
 # Export dataset
 # --------------------------------------------------------------
+
+df_cluster.to_pickle("../../data/interim/03_data_features.pkl")
